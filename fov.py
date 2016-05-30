92d34d0cb73ad227c57763daa904ed7da6a02068
Tom Fletcher <tomftom99@gmail.com>
2016-05-30 11:54:33 +0100

    updates

--- fov.py
+++ fov.py
@@ -1,39 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from mpl_toolkits.basemap import Basemap
 import asciidata
 import astropy.coordinates as coord
 import math
-
-def plot_mwd(RA,Dec,org=0,title='Mollweide projection', projection='mollweide'):
-    ''' RA, Dec are arrays of the same length.
-    RA takes values in [0,360), Dec in [-90,90],
-    which represent angles in degrees.
-    org is the origin of the plot, 0 or a multiple of 30 degrees in [0,360).
-    title is the title of the figure.
-    projection is the kind of projection: 'mollweide', 'aitoff', 'hammer', 'lambert'
-    '''
-    x = np.remainder(RA+360-org,360) # shift RA values
-    ind = x>180
-    x[ind] -=360    # scale conversion to [-180, 180]
-    x=-x    # reverse the scale: East to the left
-    tick_labels = np.array([150, 120, 90, 60, 30, 0, 330, 300, 270, 240, 210])
-    tick_labels = np.remainder(tick_labels+360+org,360)
-    fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(111, projection=projection, axisbg ='LightCyan')
-    ax.scatter(np.radians(x),np.radians(Dec))  # convert degrees to radians
-    ax.set_xticklabels(tick_labels)     # we add the scale on the x axis
-    ax.set_title(title)
-    ax.title.set_fontsize(15)
-    ax.set_xlabel("RA")
-    ax.xaxis.label.set_fontsize(12)
-    ax.set_ylabel("Dec")
-    ax.yaxis.label.set_fontsize(12)
-    ax.grid(True)
-
-
 
 df = pd.read_csv('data/PS_100701.csv')
 coords = df[['GASS', 'ra', 'dec']].values
@@ -48,40 +18,31 @@ LMass[:,0] = list(lowM[0])                         # S_CO
 LMass[:,1] = list(lowM[1])                               # z
 LMass[:,2] = list(lowM[2])                        # flag
 
-#ra = [x / 180.0 * 3.141593 for x in ra]
-
-# ax = plt.subplot(111, projection='polar')
-# ax.scatter(coords[:,1], coords[:,2], color='k',s=0.2, alpha=0.3)
-# ax.scatter(coords_L[:,1], coords_L[:,2], color='k',s=0.2, alpha=0.3)
-# ax.scatter(COLD_GASS_H[:,1], COLD_GASS_H[:,2], color='r',s=3)
-# ax.scatter(LMass[:,1], LMass[:,2], color='r',s=3)
-# #ax.set_rmax(2.0)
-# ax.grid(True)
-# plt.show()
-
-
-# r = 90
-# theta = np.arange(0,2*np.pi,0.1)
-# x = np.array([0,np.pi/3,np.pi/3,0,0])
-# y = np.array([0,0,np.pi/4,np.pi/4,0])
-# fig = plt.figure(figsize=(10, 5))
-# ax = fig.add_subplot(111, projection="mollweide", axisbg ='LightCyan')
-# ax.grid(True)
-# plot_mwd(coords[:,1], coords[:,2])
-#
-# plt.show()
-
-# ra = coord.Angle(coords[:,1].filled(np.nan)*u.degree)
-# ra = ra.wrap_at(180*u.degree)
-# dec = coord.Angle(coords[:,2].filled(np.nan)*u.degree)
 GASS = np.vstack((coords, coords_L))
 COLDGASS = np.vstack((COLD_GASS_H, LMass))
 
+x = np.array([-np.pi/3.25,-np.pi/2.86,np.pi/3,np.pi/3,-np.pi/3.25])
+y = np.array([0,np.pi/5,np.pi/5,0,0])
+
+x1 = np.array([np.pi/1.195,np.pi/1.195,np.pi,np.pi,np.pi/1.195])
+y1 = np.array([0,np.pi/120,np.pi/120,0,0])
+
+x2 = np.array([-np.pi/1.34,-np.pi/1.34,-np.pi,-np.pi,-np.pi/1.34])
+y2 = np.array([0,np.pi/120,np.pi/120,0,0])
+
 fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot(111, projection="mollweide")
-ax.scatter(((GASS[:,1]-180)*(math.pi/180.0)), (GASS[:,2]*(math.pi/180.0)), s=0.1, alpha = 0.5, label = 'GASS')
-ax.scatter(((COLDGASS[:,1]-180)*(math.pi/180.0)), (COLDGASS[:,2]*(math.pi/180.0)), s=2, color='red', label = 'COLD GASS')
+ax.scatter(((GASS[:,1]-180)*(math.pi/180.0)), (GASS[:,2]*(math.pi/180.0)), s=0.1, alpha = 0.5, label = 'SDSS')
+ax.scatter(((COLDGASS[:,1]-180)*(math.pi/180.0)), (COLDGASS[:,2]*(math.pi/180.0)), s=0.1, color='red', label = 'COLD GASS')
+
+ax.scatter(np.pi/1.195, np.pi, color='m', s=0.1)
+ax.scatter(np.pi/1.195, np.pi/1.05, color='m', s=0.1)
+
+ax.plot(x,y,color='m', linewidth=0.3)
+ax.plot(x1,y1,color='m', linewidth=0.3)
+ax.plot(x2,y2,color='m', linewidth=0.3)
 ax.grid(True)
 ax.legend(bbox_to_anchor=(0.5,1.2), loc='upper center', ncol=2, fontsize = 13)
+#ax.xaxis.set_major_formatter(plt.NullFormatter())
 fig.set_size_inches(10,6)
 plt.savefig('img/footprint.pdf', format='pdf', dpi=1000, transparent = False)
--- scal_relns.py
+++ scal_relns.py
@@ -15,39 +15,46 @@ def third(x, a, b, c, d):
 def fourth(x, a, b, c, d, e):
     return a*x*x*x*x + b*x*x*x + c*x*x + d*x + e
 
+def polyfit(data, order):
+    if order == 1:
+        fit = curve_fit(first, data[:,1], data[:,0])
+    elif order == 2:
+        fit = curve_fit(second, data[:,1], data[:,0])
+    elif order == 3:
+        fit = curve_fit(third, data[:,1], data[:,0])
+    elif order == 4:
+        fit = curve_fit(fourth, data[:,1], data[:,0])
+    return fit
 
 df = pd.read_csv('data/cold_gass_data_gio.csv')
 data = df[['Log_Mh2', 'Log_SFR', 'Log_M', 'Log_LCO']].values
 
-paramslist = []
-paramslist.append(curve_fit(first, data[:,1], data[:,0]))
-paramslist.append(curve_fit(second, data[:,1], data[:,0]))
-paramslist.append(curve_fit(third, data[:,1], data[:,0]))
-paramslist.append(curve_fit(fourth, data[:,1], data[:,0]))
-print curve_fit(first, data[:,1], data[:,0])[0]
-
+fit = polyfit(data, 3)
 x = np.linspace(-1.5,2.0,300)
-y1 = first(x, paramslist[0][0][0], paramslist[0][0][1])
-y2 = second(x, paramslist[1][0][0], paramslist[1][0][1], paramslist[1][0][2])
-y3 = third(x, paramslist[2][0][0], paramslist[2][0][1], paramslist[2][0][2], paramslist[2][0][3])
-y4 = fourth(x, paramslist[3][0][0], paramslist[3][0][1], paramslist[3][0][2], paramslist[3][0][3], paramslist[3][0][4])
+y = third(x, *fit[0])
 
-fig, ax = plt.subplots(nrows = 1, ncols = 2, squeeze=False)
+res = np.zeros((len(data[:,0]),1))
+for i in range(0, len(data[:,0])):
+    res[i,0] = data[i,0] - third(data[i,1], *fit[0])
+    print res
+
+fig, ax = plt.subplots(nrows = 2, ncols = 2, squeeze=False)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 s = ax[0,0].scatter(data[:,1], data[:,0], c=data[:,2])
-#ax[0,0].plot(x,y1, linewidth = 1)
-#ax[0,0].plot(x,y2, linewidth = 1)
-ax[0,0].plot(x,y3, linewidth = 1)
-#ax[0,0].plot(x,y4, linewidth = 5)
+ax[0,0].plot(x,y, linewidth = 1)
 ax[0,0].set_ylabel(r'$Log(MH2)$', fontsize = 12)
 ax[0,0].set_xlabel(r'$Log(SFR)$', fontsize = 12)
-cb = plt.colorbar(s)
-cb.set_label('Log M')
-################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 t = ax[0,1].scatter(data[:,1], data[:,3], c=data[:,2])
 ax[0,1].set_ylabel(r'$Log(LCO)$', fontsize = 12)
 ax[0,1].set_xlabel(r'$Log(SFR)$', fontsize = 12)
-# cb = plt.colorbar(t)
-# cb.set_label('Log M')
+cb = plt.colorbar(t)
+cb.set_label('Log M')
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ax[1,0].plot(x,res)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ax[1,1].plot(x,res)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 fig.set_size_inches(10,6)
 plt.savefig('img/scalrelns.pdf', format='pdf', dpi=1000, transparent = False)
 #plt.show()
